c25607012c0aebc335faf6c724d5844194638e42
Beasta <barryblaha@gmail.com>
2017-05-26 00:02:34 -0700

    update

--- enableIPv6.sh
+++ enableIPv6.sh
@@ -2,6 +2,6 @@
 networksetup -listallhardwareports | grep "Hardware Port: " |cut -c16- | while read line
 do
     echo "Enabling IPv6 for ${line}"
-    networksetup -setv6on "${line}"
+    networksetup -setv6automatic "${line}"
 done
 echo "IPv6 can be disabled with 'grump disableIPv6'"
